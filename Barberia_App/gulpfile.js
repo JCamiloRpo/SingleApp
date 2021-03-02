15e307806b57363b931532c2328d88e09c4d9713
J Camilo Rpo V <juan.restrepov@upb.edu.co>
2021-03-02 08:29:20 -0500

    Creación de la función para automatizar sass-css
    Falta verificar el funcionamiento

--- Barberia_App/gulpfile.js
+++ Barberia_App/gulpfile.js
@@ -1,5 +1,15 @@
-function defaultTask(cb){
-	cb();
+const {src,dest} = require('gulp');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const cleanCSS = require('gulp-clean-css');
+
+function css(){
+	return src("./sass/**/*.scss")
+			.pipe(sass())
+			.pipe(cleanCSS())
+			.pipe(rename("main.min.css"))
+			.pipe(dest("./css"));
 }
 
-exports.default= defaultTask
+exports.css = css;
+exports.default= ()=>{};
