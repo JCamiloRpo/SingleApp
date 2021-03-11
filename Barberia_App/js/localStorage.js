5e792eff212c09e5f31be37c29be194d92870ce8
juan0202 <juan.zuluagaf@upb.edu.co>
2021-03-11 01:49:26 -0500

    almacenamiento en el localStorage v1

---
+++ Barberia_App/js/localStorage.js
@@ -0,0 +1,8 @@
+var corte_txt,barbero_txt,fecha_txt;
+
+
+function asignarEventos(){
+    var btn_Reserva = document.getElementById("btnReservar");
+    btn_Reserva.addEventListener("click",almacenarReserva);
+
+}
